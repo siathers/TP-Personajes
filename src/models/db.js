9ae19d0672179235e70266308476056803203197
JUAN FERGUSON <46955772@ort>
2023-05-12 11:26:07 -0300

    prueba

---
+++ src/models/db.js
@@ -0,0 +1,12 @@
+const config = {
+    user: 'Personajes',
+    password: 'Personajes',
+    server: 'A-PHZ2-CEO-005',
+    database: 'DAI-Pizzas',
+    options: {
+        trustServerCertificate: true,
+        trustedConnection: true,
+    }
+}
+
+export default config;

1284f8212c5282bf7de6837eb79f540fb67ebb3a
JUAN FERGUSON <46955772@ort>
2023-05-19 10:38:39 -0300

    Controller

--- src/controllers/personajesContoller.js
+++ src/controllers/personajesContoller.js
@@ -7,6 +7,29 @@ router.get('/characters', async(req, res) => {
     res.status(200).send(personajes);
 });
 router.get('/characters/:id', async(req, res) => {
-    const personajes = await getAllCharacters();
-    res.status(200).send(personajes);
+    const personaje = await getCharacterById(id);
+    if(id>0){
+        res.status(200).send(personaje);
+    }
+    else if(id<=0){
+        res.status(404).send(personaje);
+    }
+});
+router.post('/characters/:id', async(req, res) => {
+    const personajes = await deleteCharacterById(id);
+    if(id>0){
+        res.status(200).send(personajes);
+    }
+    else if(id<=0){
+        res.status(404).send(personajes);
+    }
+});
+router.delete('/characters/:id', async(req, res) => {
+    const personajes = await deleteCharacterById(id);
+    if(id>0){
+        res.status(200).send(personajes);
+    }
+    else if(id<=0){
+        res.status(404).send(personajes);
+    }
 })
--- src/services/personajesService.js
+++ src/services/personajesService.js
@@ -1,28 +1,28 @@
 import sql from 'mssql';
 import configDB from '../models/db.js';
 
-export const getAll = async () => {
+export const getAllCharacters = async () => {
     const connection = await sql.connect(configDB);
     const results = await conn.request().query('SELECT * FROM Personajes');
     return results.recordset; 
 }
-export const getById = async (id) => {
+export const getCharacterById = async (id) => {
     const connection = await sql.connect(configDB);
-    const result = await conn.request().input('pId', sql.Int, id).query('SELECT * FROM Personajes where Id_Personajes = @pId');
+    const result = await conn.request().input('pId_Personaje', sql.Int, id).query('SELECT * FROM Personajes where Id_Personajes = @pId_Personaje');
     return result.recordset;
 }
-export const create = async (personaje) => {
+export const createCharacter = async (personaje) => {
     const connection = await sql.connect(configDB);
-    const result = await conn.request().input('pNombre', sql.VarChar, personaje.Nombre).input('pimagen', sql.string, personaje.Imagen).input('pEdad', sql.int, personaje.Edad).input('pPeso', sql.Int, personaje.Peso).input('pHistoria', sql.VarChar, personaje.Historia).input('pFK_Peliserie', sql.Int, personaje.FK_Peliserie).query('INSERT INTO Pizzas (Nombre, Imagen, Edad, Peso) VALUES (@pNombre, @pLibreGluten, @pImporte, @pDescripcion)');
+    const result = await conn.request().input('pNombre', sql.VarChar, personaje.Nombre).input('pimagen', sql.string, personaje.Imagen).input('pEdad', sql.int, personaje.Edad).input('pPeso', sql.Int, personaje.Peso).input('pHistoria', sql.VarChar, personaje.Historia).input('pFK_Peliserie', sql.Int, personaje.FK_Peliserie).query('INSERT INTO Pizzas (Nombre, Imagen, Edad, Peso, Historia, FK_Peliserie) VALUES (@pNombre, @pImagen, @pEdad, @pPeso, @Historia, @FK_Peliserie)');
     return result.recordset;
 }
-export const update = async (pizza, id) => {
+export const updateCharacter = async (pizza, id) => {
     const connection = await sql.connect(configDB);
-    const result = await conn.request().input('pId', sql.Int, id).input('pNombre', sql.VarChar, pizza.Nombre).input('pLibreGluten', sql.Bit, pizza.LibreGluten).input('pImporte', sql.Float, pizza.Importe).input('pDescripcion', sql.VarChar, pizza.Descripcion).query('UPDATE Pizzas SET Nombre=@pNombre, LibreGluten=@pLibreGluten, Importe=@pImporte, Descripcion=@pDescripcion WHERE Id=@pId');
+    const result = await conn.request().input('pNombre', sql.VarChar, personaje.Nombre).input('pimagen', sql.string, personaje.Imagen).input('pEdad', sql.int, personaje.Edad).input('pPeso', sql.Int, personaje.Peso).input('pHistoria', sql.VarChar, personaje.Historia).input('pFK_Peliserie', sql.Int, personaje.FK_Peliserie).input(pId_Personaje, sql.Int, id).query('UPDATE Pizzas SET Nombre=@pNombre, Imagen=@pImagen, Edad=@pEdad, Historia=@pHistoria, FK_Peliserie=@pFK_Peliserie WHERE Id_Personaje=@pId_Personaje');
     return result.recordset;
 }
-export const deleteById = async (id) => {
+export const deleteCharacterById = async (id) => {
     const connection = await sql.connect(configDB);
-    const result = await conn.request().input('pId', sql.Int, id).query('DELETE FROM Personajes where Id_Personaje = @pId');
+    const result = await conn.request().input('pId_Personaje', sql.Int, id).query('DELETE FROM Personajes where Id_Personaje = @pId_Personaje');
     return result.recordset;
 }

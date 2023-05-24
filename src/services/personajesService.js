36a7b725f7a03cd96b019ab6215bb89c00ef61d5
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-05-24 08:58:59 -0300

    sacarfk

--- src/services/personajesService.js
+++ src/services/personajesService.js
@@ -13,12 +13,12 @@ export const getCharacterById = async (id) => {
 }
 export const createCharacter = async (personaje) => {
     const connection = await sql.connect(configDB);
-    const result = await conn.request().input('pNombre', sql.VarChar, personaje.Nombre).input('pImagen', sql.string, personaje.Imagen).input('pEdad', sql.int, personaje.Edad).input('pPeso', sql.Int, personaje.Peso).input('pHistoria', sql.VarChar, personaje.Historia).input('pFK_Peliserie', sql.Int, personaje.FK_Peliserie).query('INSERT INTO Personaje (Nombre, Imagen, Edad, Peso, Historia, FK_Peliserie) VALUES (@pNombre, @pImagen, @pEdad, @pPeso, @Historia, @FK_Peliserie)');
+    const result = await conn.request().input('pNombre', sql.VarChar, personaje.Nombre).input('pImagen', sql.string, personaje.Imagen).input('pEdad', sql.int, personaje.Edad).input('pPeso', sql.Int, personaje.Peso).input('pHistoria', sql.VarChar, personaje.Historia).query('INSERT INTO Personaje (Nombre, Imagen, Edad, Peso, Historia, FK_Peliserie) VALUES (@pNombre, @pImagen, @pEdad, @pPeso, @Historia)');
     return result.recordset;
 }
 export const updateCharacter = async (personaje, id) => {
     const connection = await sql.connect(configDB);
-    const result = await conn.request().input('pNombre', sql.VarChar, personaje.Nombre).input('pImagen', sql.string, personaje.Imagen).input('pEdad', sql.int, personaje.Edad).input('pPeso', sql.Int, personaje.Peso).input('pHistoria', sql.VarChar, personaje.Historia).input('pFK_Peliserie', sql.Int, personaje.FK_Peliserie).input(pId_Personaje, sql.Int, id).query('UPDATE Personaje SET Nombre=@pNombre, Imagen=@pImagen, Edad=@pEdad, Historia=@pHistoria, FK_Peliserie=@pFK_Peliserie WHERE Id_Personaje=@pId_Personaje');
+    const result = await conn.request().input('pNombre', sql.VarChar, personaje.Nombre).input('pImagen', sql.string, personaje.Imagen).input('pEdad', sql.int, personaje.Edad).input('pPeso', sql.Int, personaje.Peso).input('pHistoria', sql.VarChar, personaje.Historia).input(pId_Personaje, sql.Int, id).query('UPDATE Personaje SET Nombre=@pNombre, Imagen=@pImagen, Edad=@pEdad, Historia=@pHistoria WHERE Id_Personaje=@pId_Personaje');
     return result.recordset;
 }
 export const deleteCharacterById = async (id) => {
